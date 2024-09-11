40c99470b4d7ae02d549ac67af0308527e9f6052
Owen Mount <96270460+fletchly@users.noreply.github.com>
2024-09-11 13:45:27 -0700

    Initial commit

---
+++ src/main/java/org/gcu/milestone/MilestoneApplication.java
@@ -0,0 +1,15 @@
+package org.gcu.milestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MilestoneApplication.class, args);
+    }
+
+}
