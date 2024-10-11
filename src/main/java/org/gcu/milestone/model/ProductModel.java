fbb6862bb2aaf2126a6cdf030682497c7fb4a3f2
Owen Mount <96270460+fletchly@users.noreply.github.com>
2024-10-11 13:15:29 -0700

    Create ProductModel.java

---
+++ src/main/java/org/gcu/milestone/model/ProductModel.java
@@ -0,0 +1,81 @@
+package org.gcu.milestone.model;
+
+/**
+ * Product model class
+ */
+public class ProductModel
+{
+    // Class-level properties
+    private long id;
+    private String name;
+    private String description;
+    private float price;
+    private int quantity;
+
+    /**
+     * Parameterized constructor
+     * @param id Product ID
+     * @param name Product name
+     * @param description Product description
+     * @param price Product price
+     * @param quantity Product quantity
+     */
+    public ProductModel(long id, String name, String description, float price, int quantity)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public float getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(float price)
+    {
+        this.price = price;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+}
