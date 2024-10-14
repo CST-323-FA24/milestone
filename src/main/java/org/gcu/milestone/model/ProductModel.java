0be7b78523a5ae973e8279630f1b9c74cb814861
Owen Mount <96270460+fletchly@users.noreply.github.com>
2024-10-13 17:26:11 -0700

    Update product model to use lombok

--- src/main/java/org/gcu/milestone/model/ProductModel.java
+++ src/main/java/org/gcu/milestone/model/ProductModel.java
@@ -1,8 +1,15 @@
 package org.gcu.milestone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Product model class
  */
+@Getter
+@Setter
+@AllArgsConstructor
 public class ProductModel
 {
     // Class-level properties
@@ -11,75 +18,4 @@ public class ProductModel
     private String description;
     private float price;
     private int quantity;
-
-    /**
-     * Parameterized constructor
-     * @param id Product ID
-     * @param name Product name
-     * @param description Product description
-     * @param price Product price
-     * @param quantity Product quantity
-     */
-    public ProductModel(long id, String name, String description, float price, int quantity)
-    {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    // --------------------------------------------
-    // Getters and Setters
-    // --------------------------------------------
-    
-    public long getId()
-    {
-        return id;
-    }
-
-    public void setId(long id)
-    {
-        this.id = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
-
-    public float getPrice()
-    {
-        return price;
-    }
-
-    public void setPrice(float price)
-    {
-        this.price = price;
-    }
-
-    public int getQuantity()
-    {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity)
-    {
-        this.quantity = quantity;
-    }
 }
