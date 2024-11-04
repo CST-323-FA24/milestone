499fdd00a9d55511f730ee495db4fb08c1ad3bed
Owen Mount <96270460+fletchly@users.noreply.github.com>
2024-11-03 22:31:35 -0700

    Merge frontend

--- src/main/java/org/gcu/milestone/controller/HomeController.java
+++ src/main/java/org/gcu/milestone/controller/HomeController.java
@@ -1,5 +1,8 @@
+package org.gcu.milestone.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -10,8 +13,8 @@ public class HomeController {
     }
 
     // Fallback for all other routes to allow React to handle client-side routing
-    @GetMapping("/**/{path:[^\\.]*}")
-    public String forwardToReact() {
+    @GetMapping("/**/{path:[^.]*}")
+    public String forwardToReact(@PathVariable String path) {
         return "forward:/index.html";
     }
 }
